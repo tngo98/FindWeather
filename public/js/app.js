f586e7d0cbbd3203e7ea1b4ed697d66950b906ba
Tommy Ngo <tommykhoango@live.com>
2021-01-04 09:38:55 -0500

    Deleted unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -52,7 +52,6 @@ weatherForm.addEventListener("submit", (e) => {
       response.json().then((data) => {
         if (data.error) {
           messageOne.textContent = data.error;
-          console.log(data.error);
         } else {
           messageOne.textContent = data.location;
           messageTwo.textContent = data.forecast;
