adb719a8132c1f46da056e003194a38d13c0c101
Tommy Ngo <tommykhoango@live.com>
2021-01-04 10:35:08 -0500

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for express config
 console.log(__dirname);
@@ -102,6 +103,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port + ".");
 });
